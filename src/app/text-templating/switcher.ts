8cff3dc2e7726abc991b73f3c45c7a0566d19b63
Jonas Gabriel Maia <jgabrielmaia@outlook.com>
2021-05-24 18:25:09 -0300

    fix: switching contexts

--- src/app/text-templating/switcher.ts
+++ src/app/text-templating/switcher.ts
@@ -2,13 +2,20 @@ import faker from "faker";
 import { ISwitcher } from "../interfaces/iswitcher";
 import { load } from "../state-management";
 
-const switchers: ISwitcher[] = load(__dirname + `/../../../conf/switches.json`);
+const switchers: ISwitcher[] = load(process.cwd() + `/conf/switches.json`);
+const casesCache: any[] = [];
 let switcherType = '';
 
 export const switcher = (name: string, caseProperty: string) => {
-    if (!switcherType) {
-        const cases = switchers.find(switcher => switcher.name == name).cases;
-        switcherType = faker.random.arrayElement(cases)[`TYPE`];
+    if ((caseProperty == 'TYPE' && switcherType != name) || switcherType == '') {
+        if (!casesCache.some(c => c.name == name)) {
+            const cases = faker.random.arrayElement(switchers.find(switcher => switcher.name == name).cases)
+            casesCache.push({ name, cases });
+            switcherType = cases['TYPE'];
+        }
+        else {
+            switcherType = casesCache.find(c => c.name == name).cases['TYPE']
+        }
     }
 
     return (
